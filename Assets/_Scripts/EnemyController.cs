31c6138354d405f99373bd566c954d2d04648659
Khilan <khilan9696@gmail.com>
2016-03-02 19:03:21 -0500

    First commit

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Speed {
+	public float minSpeed = 5f;
+	public float maxSpeed = 10f;
+}
+
+[System.Serializable]
+public class Boundary {
+	public float xMin = 290;
+	public float xMax = -290;
+	public float yMin = 300;
+	public float yMax = -180;
+}
+
+
+public class EnemyController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public Speed speed;
+	public Boundary boundary;
+
+	// PRIVATE INSTANCE VARIABLES
+	private float _CurrentSpeed;
+	private float _CurrentDrift;
+	private Transform _transform;
+
+	// Use this for initialization
+	void Start () {
+
+		this._transform = gameObject.GetComponent<Transform> ();
+		this._Reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 currentPosition = gameObject.GetComponent<Transform> ().position;
+		currentPosition.y -= this._CurrentSpeed;
+		gameObject.GetComponent<Transform> ().position = currentPosition;
+		
+		// Check bottom boundary
+		if (currentPosition.y <= boundary.yMin) {
+			this._Reset();
+		}
+	}
+
+	// resets the gameObject
+	private void _Reset() {
+		this._CurrentSpeed = Random.Range (speed.minSpeed, speed.maxSpeed);
+		Vector2 resetPosition = new Vector2 (Random.Range(boundary.xMin, boundary.xMax), boundary.yMax);
+		gameObject.GetComponent<Transform> ().position = resetPosition;
+	}
+}
