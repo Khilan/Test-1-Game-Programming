7c5d3358e8d1b826c15e87c11c2b1c714b21d9b6
Khilan <khilan9696@gmail.com>
2016-03-02 19:35:45 -0500

    Added more functionality

---
+++ Assets/_Scripts/Tankcollider.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tankcollider : MonoBehaviour {
+
+	private AudioSource[] _audioSource;
+	private AudioSource _isTank;
+	private AudioSource _enemyTank;
+
+	public GameController gamecontroller;
+
+	// Use this for initialization
+	void Start () {
+//		this._audioSource = gameObject.GetComponent<AudioSource> ();
+		this._isTank = this._audioSource [1];
+		this._enemyTank = this._audioSource [2];
+	
+	}
+	public void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.CompareTag ("Enemy")) {
+			this._isTank.Play ();
+
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+
+}
